31b009f1dd605aa84fa26320d95bcd6daf552301
mutembeijoe <mutembeijoe@gmail.com>
2020-05-09 14:53:36 +0300

    renders posts dynamically to a page

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,11 +1,28 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
 
-const BlogTemplate = ()=>(
+const BlogTemplate = props => {
+  const { data: { markdownRemark: { frontmatter: { title, date }, html} } } = props;
+  return (
     <Layout>
-        This is a blog template
+      <h1>{title}</h1>
+      <p>{date}</p>
+      <div dangerouslySetInnerHTML={{__html:html}}></div>
     </Layout>
-)
+  )
+}
 
-export default BlogTemplate;
+export default BlogTemplate
