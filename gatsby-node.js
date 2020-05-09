e423df8ab8f347b55b31a48dc0d4c876ccaaa167
mutembeijoe <mutembeijoe@gmail.com>
2020-05-09 10:16:13 +0300

    adds createPages nodeApi to create dynamic blog pages based on atemplate

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,45 @@
-const path = require('path')
+const path = require("path")
 
-module.exports.onCreateNode = ({node, actions}) =>{
-    const {createNodeField} = actions
+module.exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions
 
-    if(node.internal.type=='MarkdownRemark'){
-        const slug = path.basename(node.fileAbsolutePath, '.md')
-        createNodeField({
-            node,
-            name:"slug",
-            value:slug
-        })
-    }
-} 
+  if (node.internal.type == "MarkdownRemark") {
+    const slug = path.basename(node.fileAbsolutePath, ".md")
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    })
+  }
+}
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogTemplate = path.resolve(`./src/templates/blog.js`)
+
+  const {data:{allMarkdownRemark:{edges}}} = await graphql(
+    `
+      query {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  edges.forEach(edge => {
+      createPage({
+          path:`/blog/${edge.node.fields.slug}`,
+          component:blogTemplate,
+          context:{
+              slug:edge.node.fields.slug
+          }
+      })
+  })
+}
---
+++ src/templates/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+
+const BlogTemplate = ()=>(
+    <Layout>
+        This is a blog template
+    </Layout>
+)
+
+export default BlogTemplate;
